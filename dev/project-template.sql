8d58cb0325450479b8b0eac6fbd2779ab1d319d8
nickniebaum <nickniebaum@gmail.com>
2013-09-10 14:21:33 -0500

    Add log model

--- dev/project-template.sql
+++ dev/project-template.sql
@@ -1,92 +1,144 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- MySQL dump 10.13  Distrib 5.5.29, for osx10.6 (i386)
+--
+-- Host: localhost    Database: project_template
+-- ------------------------------------------------------
+-- Server version	5.5.29
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Table `user`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `user` ;
+--
+-- Table structure for table `log`
+--
 
-CREATE  TABLE IF NOT EXISTS `user` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `email` VARCHAR(64) NOT NULL ,
-  `password` VARCHAR(64) NOT NULL ,
-  `first_name` VARCHAR(32) NULL ,
-  `last_name` VARCHAR(32) NULL ,
-  `phone` VARCHAR(14) NULL ,
-  `phone_text_capable` TINYINT NULL DEFAULT 0 ,
-  `created_at` DATETIME NOT NULL ,
-  `updated_at` DATETIME NULL ,
-  `last_login` DATETIME NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `log` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `message` text,
+  `data` text,
+  `type` enum('log','error') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `log`
+--
 
--- -----------------------------------------------------
--- Table `role`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `role` ;
+LOCK TABLES `log` WRITE;
+/*!40000 ALTER TABLE `log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `log` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE  TABLE IF NOT EXISTS `role` (
-  `user_id` INT NOT NULL ,
-  `role` VARCHAR(32) NOT NULL ,
-  INDEX `fk_role_user_idx` (`user_id` ASC) ,
-  PRIMARY KEY (`user_id`, `role`) ,
-  CONSTRAINT `fk_role_user`
-    FOREIGN KEY (`user_id` )
-    REFERENCES `user` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Table structure for table `notification`
+--
 
+DROP TABLE IF EXISTS `notification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notification` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `description` text,
+  `vars` text NOT NULL,
+  `email_enabled` tinyint(4) NOT NULL DEFAULT '0',
+  `email_subject` text,
+  `email_message` text,
+  `sms_enabled` tinyint(4) NOT NULL DEFAULT '0',
+  `sms_message` varchar(160) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `notification`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `notification` ;
+--
+-- Dumping data for table `notification`
+--
 
-CREATE  TABLE IF NOT EXISTS `notification` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `key` VARCHAR(64) NOT NULL ,
-  `name` VARCHAR(64) NOT NULL ,
-  `description` TEXT NULL ,
-  `vars` TEXT NOT NULL ,
-  `email_enabled` TINYINT NOT NULL DEFAULT 0 ,
-  `email_subject` TEXT NULL ,
-  `email_message` TEXT NULL ,
-  `sms_enabled` TINYINT NOT NULL DEFAULT 0 ,
-  `sms_message` VARCHAR(160) NULL ,
-  `updated_at` DATETIME NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+LOCK TABLES `notification` WRITE;
+/*!40000 ALTER TABLE `notification` DISABLE KEYS */;
+INSERT INTO `notification` VALUES (1,'test_notification','Test Notification','This is the default notification upon installing the project template. An administrator should add these notifications.','[]',1,NULL,'This is the e-mail message.',1,'This is the SMS message.',NULL);
+/*!40000 ALTER TABLE `notification` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `role`
+--
 
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `user_id` int(11) NOT NULL,
+  `role` varchar(32) NOT NULL,
+  PRIMARY KEY (`user_id`,`role`),
+  KEY `fk_role_user_idx` (`user_id`),
+  CONSTRAINT `fk_role_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Dumping data for table `role`
+--
 
--- -----------------------------------------------------
--- Data for table `user`
--- -----------------------------------------------------
-START TRANSACTION;
-INSERT INTO `user` (`id`, `email`, `password`, `first_name`, `last_name`, `phone`, `phone_text_capable`, `created_at`, `updated_at`, `last_login`) VALUES (1, 'nick@lifthousedesign.com', '4f1f8def85fc3bf2dc58f04a667c8273b37a8b4c', 'Nick', 'Niebaum', '(304) 871-6066', 1, '2013-08-01 16:35:34', NULL, NULL);
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'administrator');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
 
-COMMIT;
+--
+-- Table structure for table `user`
+--
 
--- -----------------------------------------------------
--- Data for table `role`
--- -----------------------------------------------------
-START TRANSACTION;
-INSERT INTO `role` (`user_id`, `role`) VALUES (1, 'administrator');
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `first_name` varchar(32) DEFAULT NULL,
+  `last_name` varchar(32) DEFAULT NULL,
+  `phone` varchar(14) DEFAULT NULL,
+  `phone_text_capable` tinyint(4) DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-COMMIT;
+--
+-- Dumping data for table `user`
+--
 
--- -----------------------------------------------------
--- Data for table `notification`
--- -----------------------------------------------------
-START TRANSACTION;
-INSERT INTO `notification` (`id`, `key`, `name`, `description`, `vars`, `email_enabled`, `email_subject`, `email_message`, `sms_enabled`, `sms_message`, `updated_at`) VALUES (1, 'test_notification', 'Test Notification', 'This is the default notification upon installing the project template. An administrator should add these notifications.', '[]', 1, NULL, 'This is the e-mail message.', 1, 'This is the SMS message.', NULL);
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'bain.lifthousedesign@gmail.com','7505d64a54e061b7acd54ccd58b49dc43500b635','Bain','Mullins','(432) 234-2342',0,'0000-00-00 00:00:00','2013-08-26 23:09:28','2013-08-26 23:09:28');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-COMMIT;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-09-10 14:21:11
